d59c89c694204c567fb70e6c13a497ca80634dd5
Khepu <makris.giwrgos1234@gmail.com>
2019-05-28 20:44:56 +0300

    Fixed graphics function

--- js/interface.js
+++ js/interface.js
@@ -1,34 +1,9 @@
-const id = document.getElementById;
+const id = x => document.getElementById(x+"");
 const x = "./img/x.jpg";
 const o = "./img/o.jpg";
 const white = "./img/white.jpg";
 const images = [white, o, x];
-//img.src = "./img/x.jpg";
-
-/*
-JS code:
-    var img = document.createElement("img");
-    img.src = "image.png";
-    var src = document.getElementById("x");
-    src.appendChild(img);
-*/
-
-
-//x.appendChild(img);
-function img(path) {
-  const img = document.createElement('img');
-  img.src = path;
-  return img;
-}
-
 
 function graphics(state) {
-
-  let i;
-  for (i = 0; i < state.length; i++) {
-
-    id(i+"").childNodes[0].src = images[state[i]];
-
-  }
-
+    state.map((s, i) => id(i).childNodes[0].src = images[s]);
 }
--- js/main.js
+++ js/main.js
@@ -85,3 +85,4 @@ const start = () => {
     }
     return "Done";
 };
+
